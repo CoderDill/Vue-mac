04cb6ec231d985304d0ae97bbef87c85dec10ec5
Matthew Dillon <matt.dillon@launchbadge.com>
2022-03-21 21:32:38 -0700

    ActiveUser && UserData

---
+++ src/components/UserData.vue
@@ -0,0 +1,23 @@
+<template>
+  <form>
+      <label>Name</label>
+      <input type="text" v-model="name"/>
+      <label>Age</label>
+      <input type="number" v-model="age"/>
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            age: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
